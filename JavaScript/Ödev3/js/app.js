437acc0eed94eaeb4629d485ab259b4dd1516545
Kaan Colak <kaancolak3458@gmail.com>
2022-10-18 22:35:09 +0300

    updated files

--- JavaScript/Ödev3/js/app.js
+++ JavaScript/Ödev3/js/app.js
@@ -59,7 +59,7 @@ const menu = [
     category: "Korea",
     price: 15.99,
     img:
-      "https://www.curiouscuisiniere.com/wp-content/uploads/2020/04/Jajangmyeon-Korean-Noodles-in-Black-Bean-Sauce5.1200H-720x540.jpg",
+      "https://www.curiouscuisiniere.com/wp-content/uploads/2020/04/Jajangmyeon-Korean-Noodles-in-Black-Bean-Sauce5.1200H-500x500.jpg",
     desc: `Black bean sauce noodle, serving with green onion `,
   },
   {
@@ -81,3 +81,131 @@ const menu = [
     desc: `Red bean paste dessert, serving with honey.`,
   },
 ];
+
+//we can select button section from index.html
+const getButtons = document.querySelector(".btn-container")
+getButtons.innerHTML = `
+<ul class="list-inline">
+<li class="list-inline-item"><button class="btn btn-outline-dark d-inline" id="all"> All </button></li>
+<li class="list-inline-item"><button class="btn btn-outline-dark d-inline" id="korea"> Korea</button></li>
+<li class="list-inline-item"><button class="btn btn-outline-dark d-inline" id="japan"> Japan</button></li>
+<li class="list-inline-item"><button class="btn btn-outline-dark d-inline" id="china"> China</button></li>
+</ul>`
+
+//We should dived block by 2 because it is realating bootstrap grid systems
+//I mean col-6 shows two block in a row
+// Create event-click function 
+const allButton = document.querySelector("#all")
+allButton.addEventListener("click", allFunction)
+const rowDOM = document.querySelector("#foodMenu") //menu items columns
+
+function allFunction() {
+  if (rowDOM) { //if you clicked,it will be reloaded!!!
+    rowDOM.innerHTML = ``
+    menu.map((object) => { //I entered to menu lists with map!!!
+      const colDOM = document.createElement("div") //Creating div
+      colDOM.classList.add("col-sm-12", "col-lg-6", "menu-items") //I added col classes and menu items to div for responsive structure!!!
+      colDOM.innerHTML = `
+      <div>
+        <img class="photo" src="${object.img}" alt="" />
+      </div>
+      <div>
+        <h4 class="menu-title menu-info">
+          <div>${object.title}</div>
+          <div>${object.price}</div>
+        </h4>
+        <div class="menu-info menu-text">${object.desc}</div>
+      </div>
+    ` // it is relating with div and menu items rules
+      rowDOM.appendChild(colDOM) //I added colDOM to rowDOM
+    })
+  }
+}
+
+//KOREA
+const koreaButton = document.querySelector("#korea")
+koreaButton.addEventListener("click", koreaFunction)
+
+function koreaFunction() {
+  if (rowDOM) {
+    rowDOM.innerHTML = ``
+    menu.map((object) => {
+      if (object.category == "Korea") { //I chose korea catagory
+        const colDOM = document.createElement("div")
+        colDOM.classList.add("col-sm-12", "col-lg-6", "menu-items")
+        colDOM.innerHTML = `
+        <div>
+        <img class="photo" src="${object.img}" alt="" />
+      </div>
+      <div>
+        <h4 class="menu-title menu-info">
+          <div>${object.title}</div>
+          <div>${object.price}</div>
+        </h4>
+        <div class="menu-info menu-text">${object.desc}</div>
+      </div>
+        `
+        rowDOM.appendChild(colDOM)
+      }
+    })
+  }
+}
+
+//JAPAN
+
+const japanButton = document.querySelector("#japan")
+japanButton.addEventListener("click", japanFunction)
+function japanFunction() {
+  if (rowDOM) {
+    rowDOM.innerHTML = ``
+    menu.map((object) => {
+      if (object.category == "Japan") {
+        const colDOM = document.createElement("div")
+        colDOM.classList.add("col-sm-12", "col-lg-6", "menu-items")
+        colDOM.innerHTML = `
+        <div>
+          <img class="photo" src="${object.img}" alt="">
+        </div>
+        <div>
+          <h4 class="menu-title menu-info">
+            <div>${object.title}</div>
+            <div>${object.price}</div>
+          </h4>
+          <div class="menu-info menu-text">${object.desc}</div>
+        </div> 
+        `
+        rowDOM.appendChild(colDOM)
+      }
+    })
+  }
+}
+
+//CHINA
+const chinaButton= document.querySelector ("#china")
+chinaButton.addEventListener ("click",chinaFunction)
+
+function chinaFunction() {
+  if (rowDOM) {
+    rowDOM.innerHTML=``
+    menu.map ((object) => {
+      if (object.category=="China") {
+        const colDOM =document.createElement ("div")
+        colDOM.classList.add("col-sm-12", "col-md-6", "menu-items")
+        colDOM.innerHTML = `
+        <div>
+          <img class="photo" src="${object.img}" alt="">
+        </div>
+        <div>
+          <h4 class="menu-title menu-info">
+            <div>${object.title}</div>
+            <div>${object.price}</div>
+          </h4>
+          <div class="menu-info menu-text">${object.desc}</div>
+        </div>
+        `
+        rowDOM.appendChild(colDOM)
+      }
+    
+    })
+  }
+}
