9b43d535c2f9d03fe6fd123a134f433f07873489
Kaan Colak <kaancolak3458@gmail.com>
2023-01-30 16:31:18 +0300

    added context files

---
+++ React/context/src/components/Container.jsx
@@ -0,0 +1,24 @@
+
+import Button from './Button';
+import Header from './Header';
+
+import {useTheme} from '../context/ThemeContext';
+import Profile from './Profile';
+
+function Container() {
+    const { theme } = useTheme();
+    console.log(theme);
+    return (
+
+        <div className={`app ${theme}`}  >
+            <Header />
+            <hr />
+            <Button />
+            <hr />
+            <Profile />
+
+        </div>
+    )
+}
+
+export default Container;
