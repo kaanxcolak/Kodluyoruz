790217a4e7a7d45ebc5ca557bf21e9da94b44fdf
Kaan Colak <kaancolak3458@gmail.com>
2023-02-20 22:07:41 +0300

    added

---
+++ React/memo/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Header({ number, increment }) { //props
+    console.log("Header Component Re-rendered!");
+    return <div> Header - {number}
+        <br /><br />
+        <button onClick={increment }> Click </button>
+
+
+
+    </div>
+
+}
+
+export default React.memo(Header);
