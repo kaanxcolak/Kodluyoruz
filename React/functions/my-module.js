1f17a13385351667bcf94d0980a77f5c9ef47b5c
Kaan Colak <kaancolak3458@gmail.com>
2023-02-20 21:58:23 +0300

    added new file

---
+++ React/functions/my-module.js
@@ -0,0 +1,24 @@
+export default (name) => {
+    // console.log("hello " + name);
+    console.log(`hello ${name}`);
+};
+
+
+export const topla = (a, b) => a + b;
+export const cikar = (a, b) => a - b;
+
+export const text = "Text";
+export const user = {
+    name: "Ahmet",
+    surname: "Yıldız",
+};
+
+export const users = [{
+        name: "Ali",
+        surname: "kerim",
+    },
+    {
+        name: "Ayşe",
+        surname: "selim",
+    }
+];
